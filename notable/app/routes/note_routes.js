0060ce6d3ce824bbfe41676c3d3f7d9a22d42fb5
Andii Shapoval <andrii.shapoval@smile-ukraine.com>
2021-11-06 21:42:26 +0200

    aded files

---
+++ notable/app/routes/note_routes.js
@@ -0,0 +1,20 @@
+module.exports = function(app, client) {
+
+    var db = client.db('app');
+
+    app.post('/notes', (req, res)=>{
+        const note ={text: req.body.body, title: req.body.title};
+        db.collection('notes').insert(note, (err, results)=>{
+            if(err)
+            {
+                res.send({'error':"An error has occured"})
+            }
+            else
+            {
+                res.send(results)
+            }
+        });
+
+    });
+
+  };
